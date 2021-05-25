e32c2b2f00f49a4ab7577a6c3cceb4f3d20bd953
Jerome Escultura <esculturajerome@gmail.com>
2021-05-26 02:02:49 +0800

    Add Image on About page

---
+++ src/components/FullWidthText.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default ({ title, text }) => {
+  return (
+    <section className="py-md-11">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-12 col-md-10 col-lg-8 text-center">
+            <h2>{title}</h2>
+
+            <p className="fs-lg text-muted mb-7 mb-md-9">{text}</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
