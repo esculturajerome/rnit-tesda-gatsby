fe0798890342b5cc25ebe070a8205a2edc0b228a
Jerome Escultura <esculturajerome@gmail.com>
2021-05-27 23:24:05 +0800

    Rename website title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby + Netlify CMS Starter",
+    title: "Romblon National Institute of Technology",
     description:
-      "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.",
+      "Skilling Romblon thru quality TVET Delivery for Peoples Prosperity",
   },
   plugins: [
     "gatsby-plugin-react-helmet",
