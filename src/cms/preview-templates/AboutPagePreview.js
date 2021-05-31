b0db5b27cfc3ad99b7f349d21702a7f9061ac692
Jerome Escultura <esculturajerome@gmail.com>
2021-05-31 23:43:24 +0800

    Add mission and vission

--- src/cms/preview-templates/AboutPagePreview.js
+++ src/cms/preview-templates/AboutPagePreview.js
@@ -2,14 +2,18 @@ import React from "react";
 import PropTypes from "prop-types";
 import { AboutPageTemplate } from "../../templates/about-page";
 
-const AboutPagePreview = ({ entry, widgetFor, getAsset }) => {
+const AboutPagePreview = ({ entry, widgetFor }) => {
   const preview = true;
+  const data = entry.getIn(["data"]).toJS();
+  console.log(data, "entryxxxyyy");
   return (
     <AboutPageTemplate
       image={entry.getIn(["data", "image"])}
       title={entry.getIn(["data", "title"])}
       desc={entry.getIn(["data", "description"])}
-      content={widgetFor("body")}
+      mission={data.main.mission}
+      vision={data.main.vision}
+      main={data.main.blurbs || { blurbs: [] }}
       preview={preview}
     />
   );
--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -6,12 +6,16 @@ import Content, { HTMLContent } from "../components/Content";
 import { Container, Row, Col } from "react-bootstrap";
 import Gallery from "../components/Gallery";
 import PreviewCompatibleImage from "../components/PreviewCompatibleImage";
+import FullWidthText from "../components/FullWidthText";
 
 export const AboutPageTemplate = ({
   title,
   desc,
   content,
   contentComponent,
+  mission,
+  vision,
+  main,
   image,
   preview,
 }) => {
@@ -26,18 +30,45 @@ export const AboutPageTemplate = ({
             <p className="lead text-muted mb-7 mb-md-9">{desc}</p>
           </Col>
         </Row>
-        {!preview && (
-          <Row>
-            <Gallery />
-          </Row>
-        )}
-      </Container>
 
-      <Container className="py-md-11 about">
+        <Row>
+          {!preview ? (
+            <Gallery />
+          ) : (
+            <p className="text-center italic">Team Gallery here</p>
+          )}
+        </Row>
+      </Container>
+      <div className="bg-gradient-light-white">
+        <Container className="py-md-11">
+          <Row>
+            <FullWidthText title="Mission Statement" text={mission} />
+          </Row>
+          <Row className="mb-5">
+            <FullWidthText title="Vision Statement" text={vision} />
+          </Row>
+          <Row className="mb-4 mt-5">
+            <Col xs={12} md={4} className="mt-2">
+              <h3>Philosophy</h3>
+              <p className="text-muted mb-6 mb-md-0">{main.philosophy}</p>
+            </Col>
+            <Col xs={12} md={4} className="mt-2">
+              <h3>Quality Policy</h3>
+              <p className="text-muted mb-6 mb-md-0">{main.quality}</p>
+            </Col>
+            <Col xs={12} md={4} className="mt-2">
+              <h3>Values Statement</h3>
+              <p className="text-muted mb-6 mb-md-0">{main.values}</p>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+
+      {/* <Container className="py-md-11 about">
         <Row>
           <PageContent className="content" content={content} />
         </Row>
-      </Container>
+      </Container> */}
       <Container className="mt-5">
         <Row>
           <h2 className="display-4 mb-4 text-center">Organizational Chart</h2>
@@ -53,8 +84,9 @@ export const AboutPageTemplate = ({
 AboutPageTemplate.propTypes = {
   title: PropTypes.string.isRequired,
   image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-  content: PropTypes.string,
-  contentComponent: PropTypes.func,
+  mission: PropTypes.string,
+  vision: PropTypes.string,
+  main: PropTypes.object,
 };
 
 const AboutPage = ({ data }) => {
@@ -62,11 +94,12 @@ const AboutPage = ({ data }) => {
   return (
     <Layout>
       <AboutPageTemplate
-        contentComponent={HTMLContent}
         title={post.frontmatter.title}
         desc={post.frontmatter.description}
         image={post.frontmatter.image}
-        content={post.html}
+        mission={post.frontmatter.main.mission}
+        vision={post.frontmatter.main.vision}
+        main={post.frontmatter.main.blurbs}
       />
     </Layout>
   );
@@ -81,10 +114,18 @@ export default AboutPage;
 export const aboutPageQuery = graphql`
   query AboutPage($id: String!) {
     markdownRemark(id: { eq: $id }) {
-      html
       frontmatter {
         title
         description
+        main {
+          mission
+          vision
+          blurbs {
+            philosophy
+            quality
+            values
+          }
+        }
         image {
           childImageSharp {
             fluid(maxWidth: 2048, quality: 100) {
