3de14b2bb4193192fc7458bc9020e27cb9cb6ca5
Jerome Escultura <esculturajerome@gmail.com>
2021-06-10 02:30:35 +0800

    Add data on tables

--- src/data/programsData.js
+++ src/data/programsData.js
@@ -22,5 +22,81 @@ export const programsData = {
       accnumber: "AC-ATS0117591921102",
       exp: "February 07, 2021",
     },
+    {
+      qualifications: "Automotive Servicing NC II ",
+      accnumber: "AC- ATS0217591921103",
+      exp: "February 07, 2021",
+    },
+     {
+      qualifications: "Bread and Pastry Production NC II  ",
+      accnumber: "AC- BPP0217591921104",
+      exp: "February 07, 2021",
+    },
+     {
+      qualifications: "Carpentry NC II  ",
+      accnumber: "AC-ROC0217591921105",
+      exp: "February 07, 2021",
+    },
+    {
+      qualifications: "Dressmaking NC II  ",
+      accnumber: "AC-DRM0217591921106",
+      exp: "February 07, 2021",
+    },
+    {
+      qualifications: "Driving NC II  ",
+      accnumber: "AC-DRV0217591921107",
+      exp: "February 07, 2021",
+    },
+     {
+      qualifications: "Electrical Installation and Maintenance NC II   ",
+      accnumber: "AC-EIM0217591921108",
+      exp: "February 07, 2021",
+    },
+     {
+      qualifications: "Food and Beverage Services NC II  ",
+      accnumber: "AC-FBS0217591921109",
+      exp: "February 07, 2021",
+    },
+     {
+      qualifications: "Food Processing NC II    ",
+      accnumber: "AC-FOP0217591921110",
+      exp: "February 07, 2021",
+    },
+     {
+      qualifications: "Land-based Transport Refrigeration Servicing NC II    ",
+      accnumber: "AC-TRS0217591820113",
+      exp: "August 14, 2020",
+    },
+     {
+      qualifications: "Masonry NC II  ",
+      accnumber: "AC-MAS0217591921111",
+      exp: "February 07, 2021",
+    },
+     {
+      qualifications: "Motorcycle/Small Engine Servicing NC II  ",
+      accnumber: "AC-MSE0217591921112",
+      exp: "February 07, 2021",
+    },
+     {
+      qualifications: "RAC (DomRAC) Servicing NC II  ",
+      accnumber: "AC-WAC0217591921113",
+      exp: "February 07, 2021",
+    },
+    {
+      qualifications: "Shielded Metal Arc Welding NC I  ",
+      accnumber: "AC-EAW0117591921114",
+      exp: "February 07, 2021",
+    },
+    {
+      qualifications: "Shielded Metal Arc Welding NC II  ",
+      accnumber: "AC-EAW0217591921115",
+      exp: "February 07, 2021",
+    },
+     {
+      qualifications: "Trainers Methodology Level I  ",
+      accnumber: "AC-TRM0117591921116",
+      exp: "February 07, 2021",
+    },
+  ],
   ],
 };
